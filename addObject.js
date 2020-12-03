10709de27624d32078a1fef590d923451d6b914c
sivaneshrusselraj <75260690+sivaneshrusselraj@users.noreply.github.com>
2020-12-03 19:05:49 +0530

    Add files via upload

---
+++ addObject.js
@@ -0,0 +1,9 @@
+function addObject(name,age){
+    this.Name = name;
+    this.Age = age;
+    this.Nationality;
+}
+var student1 = new addObject[{'name':'sivanesh','age':21}];
+var student2 = new addObject[{'name':'kesav','age':16}];
+student1.push({'nationality':'Indian'});
+student2.push({'nationality':'italy'});
